b234c54f164c96b6a471f228a083ab35cbf95a49
zq1108 <zq1108@DESKTOP-SA9EI5I>
2019-08-06 23:34:56 +0800

    update

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		// 入口运行类
+		SpringApplication.run(App.class, args);
+
+	}
 }
